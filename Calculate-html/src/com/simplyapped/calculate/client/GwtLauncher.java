1b85a4c345ce4fd912a2cdfad07fbf91047eeadd
Phil Barr <philbarrmail@gmail.com>
2013-03-27 14:04:35 +0000

    added

---
+++ Calculate-html/src/com/simplyapped/calculate/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package com.simplyapped.calculate.client;
+
+import com.simplyapped.calculate.CalculateGame;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new CalculateGame();
+	}
+}
