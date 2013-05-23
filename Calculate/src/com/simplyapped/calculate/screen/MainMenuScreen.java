2f46a91b1045485e216823fc3aad5bd9f0523002
Phil Barr <philbarrmail@gmail.com>
2013-05-23 23:22:54 +0100

    fixed main menu layout

--- Calculate/src/com/simplyapped/calculate/screen/MainMenuScreen.java
+++ Calculate/src/com/simplyapped/calculate/screen/MainMenuScreen.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
@@ -15,48 +15,51 @@ import com.simplyapped.calculate.CalculateGame;
 public class MainMenuScreen implements Screen{
 	private Stage ui;
 	private Table window;
-	private Skin uiSkin = new Skin(Gdx.files.internal("data/uiskin.json"));
+	private Skin uiSkin = new Skin(Gdx.files.internal("data/calculate.json"));
 	
 	public MainMenuScreen(final CalculateGame game) {
 	    ui = new Stage(Gdx.graphics.getWidth(),Gdx.graphics.getHeight(), true);
 	    Gdx.input.setInputProcessor(ui);
 	    window = new Table();
+	    
 	    window.setFillParent(true);
 	    window.setX(0);
 	    window.setY(0);
 	    window.debug();
 	    
+	    // calculate width and heights for the table
+	    float emptyRowHeight = Gdx.graphics.getHeight() / 10;
+	    float logoHeight = Gdx.graphics.getHeight() / 4;
+	    float logoWidth = Gdx.graphics.getWidth() / 1.1f;
+	    float buttonHeight = Gdx.graphics.getHeight() / 6;
+	    float buttonWidth = Gdx.graphics.getWidth() / 1.25f;
+	    
 	    // title label
-	    Label title = new Label("Calculate", uiSkin);
+	    Image logo = new Image(uiSkin, "calculatelogo");
 	    
 	    // buttons
-	    Button newGame = new Button(uiSkin, "default");
-	    newGame.addListener(new ClickListener() {
+	    Button playMenu = new Button(uiSkin, "play");
+	    playMenu.addListener(new ClickListener() {
 	        @Override
 	        public void clicked(InputEvent event, float x, float y)
 	        {
 	        	game.setScreen(game.gameScreen);
 	        }
 	    });
-	    Button optionMenu = new Button(uiSkin, "default");
-	    Button helpMenu = new Button(uiSkin, "default");
+	    Button optionMenu = new Button(uiSkin, "options");
+	    Button tutorialMenu = new Button(uiSkin, "tutorial");
+
+//	    window.row().padTop(emptyRowHeight);
+	    window.add(logo).width(logoWidth).height(logoHeight);
+	    window.row().expandY();
+	    window.add(playMenu).width(buttonWidth).height(buttonHeight);
+	    window.row().expandY();
+	    window.add(optionMenu).width(buttonWidth).height(buttonHeight);
+	    window.row().expandY();
+	    window.add(tutorialMenu).width(buttonWidth).height(buttonHeight);
+	    window.row().padTop(emptyRowHeight);
+	    window.setBackground(uiSkin.getTiledDrawable("graphytile"));
 	    
-	    window.row().fill(false,false).expand(true,false).padTop(50).padBottom(50);
-	    window.add(title);
-	    Table container = new Table(uiSkin);
-	    container.row().fill(true, true).expand(true, true).pad(10, 0, 10, 0);
-	    container.add(newGame);
-	    container.row().fill(true, true).expand(true, true).pad(10, 0, 10, 0);
-	    container.add(optionMenu);
-	    container.row().fill(true, true).expand(true, true).pad(10, 0, 10, 0);
-	    container.add(helpMenu);
-	    window.row().fill(0.5f,1f).expand(true,false);
-	    window.add(container);
-	    Table extras = new Table(uiSkin);
-	    extras.setColor(55, 55,55,55);
-	    extras.row().fill(false,false).expand(true,true);
-	    window.row().fill(true,false).expand(true,true);
-	    window.add(extras).bottom();
 	    ui.addActor(window);
 	    Gdx.input.setInputProcessor(ui);
 	}
@@ -67,7 +70,7 @@ public class MainMenuScreen implements Screen{
 	    Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 	    ui.act();
 	    ui.draw();
-	    Table.drawDebug(ui);
+//	    Table.drawDebug(ui);
 	}
 	@Override
 	public void resize(int width, int height) {
