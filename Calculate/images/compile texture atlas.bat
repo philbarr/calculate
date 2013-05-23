7a484d4362b928d84df0924b6e017edc523eef81
Phil Barr <philbarrmail@gmail.com>
2013-05-23 23:23:44 +0100

    added images folder and textureatlas stuff

---
+++ Calculate/images/compile texture atlas.bat
@@ -0,0 +1,2 @@
+SET LIBGDX="C:\Development\Java\lib\libgdx-0.9.7"
+java -cp %LIBGDX%\gdx.jar;%LIBGDX%\extensions\gdx-tools.jar com.badlogic.gdx.tools.imagepacker.TexturePacker2 atlas_src atlas_out calculate
