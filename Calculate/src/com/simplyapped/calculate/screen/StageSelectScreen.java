680e7c8600edaf2a0eac6ae3084185de3e35822f
Phil Barr <philbarrmail@gmail.com>
2013-06-08 01:52:45 +0100

    sd

---
+++ Calculate/src/com/simplyapped/calculate/screen/StageSelectScreen.java
@@ -0,0 +1,52 @@
+package com.simplyapped.calculate.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.simplyapped.calculate.CalculateGame;
+
+public class StageSelectScreen extends DefaultScreen
+{
+	private Table window;
+	private Skin skin = new Skin(Gdx.files.internal("data/stageselectscreen.json"));
+
+	public StageSelectScreen(DefaultGame game)
+	{
+		super(game);
+	}
+
+	@Override
+	public void show()
+	{
+		stage = new Stage(CalculateGame.SCREEN_WIDTH, CalculateGame.SCREEN_HEIGHT, false);
+		stage.addListener(new ClickListener()
+		{
+			@Override
+			public boolean keyDown(InputEvent event, int keycode)
+			{
+				if (keycode == Keys.BACK)
+				{
+					game.transitionTo(CalculateGame.MAIN_MENU_SCREEN, false);
+					return true;
+				}
+				return false;
+			}
+		});
+		
+	    window = new Table();	    
+	    window.setFillParent(true);
+	    window.setX(0);
+	    window.setY(0);
+	    window.debug();
+	    
+	    window.setBackground(skin.getDrawable("gamescreenbackground"));
+	    stage.addActor(window);
+	    Gdx.input.setInputProcessor(stage);
+	    Gdx.input.setCatchBackKey(true);
+	}
+
+}
