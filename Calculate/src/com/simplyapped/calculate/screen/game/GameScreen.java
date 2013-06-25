d0ea0eea468b6295330091ae4135fd41dc070dac
Phil Barr <philbarrmail@gmail.com>
2013-06-25 16:56:14 +0100

    gamescreen

--- Calculate/src/com/simplyapped/calculate/screen/game/GameScreen.java
+++ Calculate/src/com/simplyapped/calculate/screen/game/GameScreen.java
@@ -2,13 +2,14 @@ package com.simplyapped.calculate.screen.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 import com.simplyapped.calculate.CalculateGame;
 import com.simplyapped.libgdx.ext.DefaultGame;
 import com.simplyapped.libgdx.ext.action.TransitionFixtures;
@@ -17,7 +18,7 @@ import com.simplyapped.libgdx.ext.screen.DefaultScreen;
 public class GameScreen extends DefaultScreen
 {
 	private Table window;
-	private Skin uiSkin = new Skin(Gdx.files.internal("data/gamescreen.json"));
+	private Skin skin = new Skin(Gdx.files.internal("data/gamescreen.json"));
 
 	public GameScreen(DefaultGame game)
 	{
@@ -48,22 +49,40 @@ public class GameScreen extends DefaultScreen
 	    window.setY(0);
 	    window.debug();
 	    
-	    Table table = new Table();
-	    table.setColor(Color.WHITE);
-	    table.row();
-	    Skin skin = new Skin(Gdx.files.internal("data/uiskin.json"));
-	    Label label = new Label("asfd", skin);
-	    label.setFillParent(true);
-		table.add(label);
-	    table.size(CalculateGame.SCREEN_HEIGHT/2);
-	    table.debug();
+	    Table graph = new Table();
+	    graph.setBackground(skin.getTiledDrawable("graphtile"));
+	    graph.setWidth(CalculateGame.SCREEN_WIDTH/.8f);
+	    graph.setHeight(CalculateGame.SCREEN_HEIGHT/.4f);
 	    
-	    window.row();
-	    window.add(table);
+	    Label label = getLabel();
 	    
-	    window.setBackground(uiSkin.getDrawable("gamescreenbackground"));
+	    graph.row().expand();
+	    graph.add(getLabel());
+	    graph.row().expand();
+	    graph.add(getLabel());
+	    graph.row().expand();
+	    graph.add(getLabel());
+	    graph.row().expand();
+	    graph.add(getLabel());graph.row().expand();
+	    graph.add(getLabel());
+	    graph.row().expand();
+	    graph.add(getLabel());
+	    
+	    ScrollPane pane = new ScrollPane(graph);
+	    pane.getStyle().background = new NinePatchDrawable( skin.getPatch("calcbackground"));
+	    pane.setWidth(CalculateGame.SCREEN_WIDTH/1.2f);
+	    pane.setHeight(CalculateGame.SCREEN_HEIGHT/2.2f);
+	    pane.setPosition(CalculateGame.SCREEN_WIDTH/2 - pane.getWidth()/2, CalculateGame.SCREEN_HEIGHT - CalculateGame.SCREEN_HEIGHT/1.8f);
+	    
+	    window.setBackground(skin.getDrawable("gamescreenbackground"));
 	    stage.addActor(window);
+	    stage.addActor(pane);
 	    Gdx.input.setInputProcessor(stage);
 	    Gdx.input.setCatchBackKey(true);
+	}
+
+	private Label getLabel()
+	{
+		return new Label("blah ablah", skin, "calc");
 	}
 }
