7d2d1bb78d903643459ee7cd4fba49d74029a68b
Phil Barr <philbarrmail@gmail.com>
2013-12-22 12:51:38 +0000

    Signed-off-by: Phil Barr <philbarrmail@gmail.com>

--- Calculate-android/src/com/simplyapped/calculate/CalculateGameActivity.java
+++ Calculate-android/src/com/simplyapped/calculate/CalculateGameActivity.java
@@ -1,8 +1,21 @@
 package com.simplyapped.calculate;
 
+import java.io.ByteArrayInputStream;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import javax.security.auth.x500.X500Principal;
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.Signature;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.android.vending.billing.AndroidBillingService;
 import com.badlogic.gdx.backends.android.AndroidApplication;
@@ -29,7 +42,12 @@ public class CalculateGameActivity extends AndroidApplication {
         billing = new AndroidBillingService(this);
 		calculateGame.setBilling(billing);
         calculateGame.setDialog(new AndroidOSDialog(this));
-		initialize(calculateGame, cfg);
+		
+        
+        initialize(calculateGame, cfg);
+		
+        Log.d(CalculateGameActivity.class.toString(), "Debug? " + isDebuggable(this));
+		
     }
 	
 	@Override
@@ -42,4 +60,36 @@ public class CalculateGameActivity extends AndroidApplication {
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
+	
+	private static final X500Principal DEBUG_DN = new X500Principal("CN=Android Debug,O=Android,C=US");
+	private boolean isDebuggable(Context ctx)
+	{
+	    boolean debuggable = false;
+
+	    try
+	    {
+	        PackageInfo pinfo = ctx.getPackageManager().getPackageInfo(ctx.getPackageName(),PackageManager.GET_SIGNATURES);
+	        Signature signatures[] = pinfo.signatures;
+
+	        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+
+	        for ( int i = 0; i < signatures.length;i++)
+	        {   
+	            ByteArrayInputStream stream = new ByteArrayInputStream(signatures[i].toByteArray());
+	            X509Certificate cert = (X509Certificate) cf.generateCertificate(stream);       
+	            debuggable = cert.getSubjectX500Principal().equals(DEBUG_DN);
+	            if (debuggable)
+	                break;
+	        }
+	    }
+	    catch (NameNotFoundException e)
+	    {
+	        //debuggable variable will remain false
+	    }
+	    catch (CertificateException e)
+	    {
+	        //debuggable variable will remain false
+	    }
+	    return debuggable;
+	}
 }
